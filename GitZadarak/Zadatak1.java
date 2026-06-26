3703c03112c9fd423e2c006d634b10ec84651c63
Mata Strangar <matastrangar0@gmail.com>
2026-06-26 19:45:57 +0200

    Nova grana

--- GitZadarak/Zadatak1.java
+++ GitZadarak/Zadatak1.java
@@ -1,6 +1,6 @@
 public class Zadatak1 {
  public static void main(String[] args){
 
-System.out.println("ovo je git project nove grane");
+System.out.println("ovo je git project: branch dev");
 }
 }	
